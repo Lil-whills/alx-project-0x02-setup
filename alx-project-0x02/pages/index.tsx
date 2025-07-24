122c6eac1b1e22e5e8ee3859ef5cd1ecd2c40341
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-07-24 11:00:30 -0400

    initial commit

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl text-blue-400 font-bold">Welcome to the ALX Next.js Setup</h1>
+    </div>
+  );
+}
