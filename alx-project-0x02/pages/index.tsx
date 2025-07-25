24f77406a34bcdc339412f97d209369bd9df8e5c
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-07-24 21:22:25 -0400

    tsk2 completed

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="text-center mt-10">
       <h1 className="text-3xl text-blue-400 font-bold">Welcome to the ALX Next.js Setup</h1>
     </div>
-    <Header></Header>
+    <Header/>
     </>
     
   );
